0bdf5b4146450cebc1d3c1bf3fad6d069815013e
rajatsajwan <rajatsajwan1998@gmail.com>
2023-05-25 10:24:11 +0530

    check subscriber exist

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -4,17 +4,35 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Subscriber;
 
 class ApiController extends Controller
 {
     public function subscriber(Request $request)
     {
-        // $name = $request->query("name");
-        // $email = $request->query("Email");
-        // $phone = $request->query("phoneno");
-        // $list = $request->query("list");
+        $name = $request->query("name");
+        $email = $request->query("Email");
+        $phone = $request->query("phoneno");
+        $list = $request->query("list");
+    
+        $emails = Subscriber::pluck('email'); 
+
+        if ($emails->contains($email)) {
         
+        
+
+
+
+
+        } else {
+
+        }
+
+
+
         return response()->json([
             "message" => "Data received successfully"], 200);
     }
+
+
 }
