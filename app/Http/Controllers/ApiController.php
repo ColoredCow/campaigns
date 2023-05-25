e1654a63441153cc8a006bf9b03d0239d76b61ad
rajatsajwan <rajatsajwan1998@gmail.com>
2023-05-25 17:07:54 +0530

    more optimize code

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 use App\Models\Subscriber;
+use App\Models\Lists;
 use Illuminate\Support\Facades\DB;
 
 class ApiController extends Controller
@@ -14,12 +14,13 @@ class ApiController extends Controller
         $name = $request->query("name");
         $email = $request->query("Email");
         $phone = $request->query("phone");
-        $lists = $request->query("list");
-    
-        $emails = Subscriber::pluck('email'); 
-        $names = Subscriber::pluck('name'); 
 
-        if ($emails->contains($email) && $names->contains($name)) {
+        $stringlists = $request->query("list");
+        $lists = explode(',', $stringlists);
+
+        $emails = Subscriber::pluck('email'); 
+        dd($emails);
+        if ($emails->contains($email)) {
             foreach ($lists as $list) {
                 $this->addSubscriberToList($list, $email);
             }
@@ -45,8 +46,8 @@ class ApiController extends Controller
 
     public function addSubscriberToList($list, $email) {
 
-        $list = DB::table('lists')->where('name', $list)->first();
-        if ($list) {
+        $existingList = DB::table('lists')->where('name', $list)->first();
+        if ($existingList) {
             $subscriber = Subscriber::where('email', $email)->first();
             $subscriberId = $subscriber->id;
             $listSubscriber = DB::table('list_subscriber')->where('subscriber_id', $subscriberId)->first();
@@ -58,14 +59,17 @@ class ApiController extends Controller
             }
 
         } else {
-            DB::table('list')->insert([
+            Lists::create([
                 'name' => $list,
             ]);
+            // DB::table('list')->insert([
+            //     'name' => $list,
+            // ]);
+
             $subscriber = Subscriber::where('email', $email)->first();
             $list = DB::table('lists')->where('name', $list)->first();
-            $this->addingSubscriberToList($list->id, $subscriber->id); // this need to be optimize
+            $this->addingSubscriberToList($list->id, $subscriber->id);
         }
-
         return;
     }
 
@@ -74,6 +78,7 @@ class ApiController extends Controller
             'list_id' => $listid,
             'subscriber_id' =>$subscriberId,
         ]);
+
         return;
     }
 }
