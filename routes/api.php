4bc315a874628b48d0845d76490116d9824ba584
rajatsajwan <rajatsajwan1998@gmail.com>
2023-05-29 10:39:16 +0530

    minor change

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ApiController;
 use Aws\Api\ApiProvider;
 use App\Http\Controllers\SubscriberController;
 
