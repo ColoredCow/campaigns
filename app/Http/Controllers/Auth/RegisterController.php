92cd2a392586f9b43ef18769cdb4281c2c247005
rajatsajwan <rajatsajwan1998@gmail.com>
2023-06-07 15:35:12 +0530

    minor changes

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -69,7 +69,6 @@ class RegisterController extends Controller
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
-
         return auth()->user();
     }
 
