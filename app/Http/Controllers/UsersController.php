ec292a2c223cb4f6731c315e21901dcef8ffadd3
rajatsajwan <rajatsajwan1998@gmail.com>
2023-06-09 18:57:07 +0530

    user in order

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -10,7 +10,7 @@ class UsersController extends Controller
     public function index()
     {
         return view('users.index')->with([
-            'users' => User::all(),
+            'users' => User::orderBy('name')->get(),
         ]);
     }
 }
