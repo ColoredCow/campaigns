14738bc972bccd5a04739193cf868a82b3fe9082
rajatsajwan <rajatsajwan1998@gmail.com>
2023-06-10 14:48:41 +0530

    did changes

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,9 +9,8 @@ class UsersController extends Controller
 {
     public function index()
     {
-        $numberOfUsers = config('constants.paginate_value.paginate_value_for_user');
         return view('users.index')->with([
-            'users' => User::orderBy('name')->paginate($numberOfUsers),
+            'users' => User::orderBy('name')->paginate(config('constants.paginate_value.paginate_value_for_user')),
         ]);
     }
 }
