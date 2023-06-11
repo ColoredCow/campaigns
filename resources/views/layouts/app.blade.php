f62fc99858c35bccad562140a6a76c3f537bb27d
rajatsajwan <rajatsajwan1998@gmail.com>
2023-06-11 12:06:25 +0530

    remove Create User

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,11 +40,6 @@
                             <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                         </li>
                     @else
-                        <li class="nav-item">
-                            @if (Route::has('register'))
-                                <a class="nav-link" href="{{ route('register') }}">{{ __('Create User') }}</a>
-                            @endif
-                        </li>
                         <li class="nav-item dropdown">
                             <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                 {{ Auth::user()->name }} <span class="caret"></span>
