8b5afc1c294e92fe3644293b761342ce345651bb
Vaibhav Rathore <vaibhav@coloredcow.com>
2023-06-17 19:46:49 +0530

    formatting fix

--- app/Http/Controllers/Api/SubscriberController.php
+++ app/Http/Controllers/Api/SubscriberController.php
@@ -10,17 +10,11 @@ use App\Http\Requests\Api\SubscriberRequest;
 
 class SubscriberController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index(): Response
     {
         return response(Subscriber::paginate(100));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(SubscriberRequest $request): Response
     {
         $validated = $request->validated();
@@ -28,17 +22,11 @@ class SubscriberController extends Controller
         return response(Subscriber::find($subscriber->id));
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(Subscriber $subscriber): Response
     {
         return response($subscriber);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(SubscriberRequest $request, Subscriber $subscriber): Response
     {
         $validated = $request->validated();
@@ -46,9 +34,6 @@ class SubscriberController extends Controller
         return response($subscriber);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(Subscriber $subscriber): Response
     {
         $subscriber->delete();
