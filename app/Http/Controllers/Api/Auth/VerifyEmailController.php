5f4709a4c6c39f07852ca7d1c256e861e8583458
Vaibhav Rathore <vaibhav@coloredcow.com>
2023-06-22 17:57:08 +0530

    fix email verification route

--- app/Http/Controllers/Api/Auth/VerifyEmailController.php
+++ app/Http/Controllers/Api/Auth/VerifyEmailController.php
@@ -15,15 +15,7 @@ class VerifyEmailController extends Controller
      */
     public function __invoke(EmailVerificationRequest $request): RedirectResponse
     {
-        if ($request->user()->hasVerifiedEmail()) {
-            return redirect()->intended(
-                config('app.frontend_url').RouteServiceProvider::HOME.'?verified=1'
-            );
-        }
-
-        if ($request->user()->markEmailAsVerified()) {
-            event(new Verified($request->user()));
-        }
+        $request->fulfill();
 
         return redirect()->intended(
             config('app.frontend_url').RouteServiceProvider::HOME.'?verified=1'
