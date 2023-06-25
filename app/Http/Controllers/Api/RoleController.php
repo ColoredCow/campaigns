fde1ebf39ada095b2a6fc1b6624b71ccc17c82e2
Ayush8923 <ayush.uniyal@coloredcow.in>
2023-06-25 22:28:42 +0530

    :art: Code cleanup and remove the unwanted code

--- app/Http/Controllers/Api/RoleController.php
+++ app/Http/Controllers/Api/RoleController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Models\Role;
+use Spatie\Permission\Models\Role;
 use App\Http\Requests\Api\RolesRequest;
 use Spatie\Permission\Models\Permission;
 
