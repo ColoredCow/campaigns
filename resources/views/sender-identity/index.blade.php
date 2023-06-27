aaf9c58face37d4de05708b77800efd68f627f95
rajatsajwan <rajatsajwan1998@gmail.com>
2023-06-27 09:37:35 +0530

    remove senderidentity index file

--- resources/views/sender-identity/index.blade.php
+++ resources/views/sender-identity/index.blade.php
@@ -4,29 +4,6 @@
 
 <div class="container">
 
-    <div class="modal fade" id="deleteIdentityModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h3 class="modal-title fs-5" id="exampleModalLabel">Delete Sender-identity</h3>
-                </div>
-                    <form 
-                    action="{{ route('sender-identity.destroy') }}"
-                    method="POST">
-                        @csrf
-                        <div class="modal-body">
-                            <input type="hidden" name="id" id='identityId'>
-                            <h4>Are you sure you want to delete '<span id="displayIdentityName"></span>'?</h4>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                            <button type="submit" class="btn btn-danger">Delete</button>
-                        </div>
-                    </form>
-            </div>
-        </div>
-    </div>
-
     <div class="mb-3 d-flex align-items-center w-100">
         <h2 class="mb-0 d-flex align-items-end"><i data-feather="at-sign" class="mr-2 page-icon"></i>Sender Identities</h2>
     </div>
@@ -71,9 +48,13 @@
                             <span class="badge badge-primary mr-2">Default</span>
                         @endif
                         <a href="{{route('sender-identity.edit', $identity)}}" class="text-grey-dark mr-2" title="Edit"><i data-feather="edit" class="w-20 h-20"></i></a>
-                        <button type="submit" class="deleteIdentityBtn btn btn-link text-danger p-0" identityid="{{($identity->id)}}" identityname="{{$identity->name}}" title="Delete">
-                            <i data-feather="trash-2" class="w-20 h-20"></i>
-                        </button>
+                        <form action="{{ route('sender-identity.destroy', $identity->id) }}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-link text-danger p-0" title="Delete">
+                                <i data-feather="trash-2" class="w-20 h-20"></i>
+                            </button>
+                        </form>
                     </td>
                 </tr>
                 @endforeach
