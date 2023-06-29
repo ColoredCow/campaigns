6ea18d5874d14ca5574315c0cad7c56f29a55743
Ayush8923 <ayush.uniyal@coloredcow.in>
2023-06-29 12:20:45 +0530

    :art: make the update user API

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -4,12 +4,26 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
     public function index(): Response
     {
         return response(User::paginate(100));
+    }
+
+    public function update(Request $request, User $user)
+    {
+        $updatedPassword = Hash::make($request->password);
+        $user->update([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => $updatedPassword,
+        ]);
+
+        return response()->json(['status' => 'User updated successfully']);
     }
 }
