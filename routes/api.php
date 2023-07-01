964ae13d865d051fbc7536884547c8b931cc8779
Ayush8923 <ayush.uniyal@coloredcow.in>
2023-07-02 00:02:56 +0530

    :art: fix the user api error

--- routes/api.php
+++ routes/api.php
@@ -30,7 +30,7 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::resource('sender-identity', SenderIdentityController::class)->except(['create', 'edit']);
     Route::resource('campaign', CampaignController::class)->except(['create', 'edit']);
     Route::resource('role', RoleController::class)->except(['create', 'edit']);
-    Route::resource('user', UserController::class)->except(['create', 'edit']);
+    Route::resource('user/user', UserController::class)->except(['create', 'edit']);
     Route::post('user/update-user-role', [UserController::class, 'updateUserRoles'])->name('user.updateUserRoles');
     Route::post('role/update-role-permission', [RoleController::class, 'updateRolePermissions'])->name('role.updateRolePermissions');
 });
