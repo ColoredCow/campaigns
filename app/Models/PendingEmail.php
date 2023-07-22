7e9effbd0c4f90742d2dd8220a8fef042268b4d4
Ayush8923 <ayush.uniyal@coloredcow.in>
2023-07-22 21:05:36 +0530

    :fire: remove unused code and add the some changes

--- app/Models/PendingEmail.php
+++ app/Models/PendingEmail.php
@@ -3,18 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class PendingEmail extends Model
 {
     protected $guarded = [];
 
-    public function campaign()
+    public function campaign(): BelongsTo
     {
-        return $this->belongsTo(Campaign::class, 'campaign_id');
+        return $this->belongsTo(Campaign::class);
     }
 
-    public function subscriber()
+    public function subscriber(): BelongsTo
     {
-        return $this->belongsTo(Subscriber::class, 'subscriber_id');
+        return $this->belongsTo(Subscriber::class);
     }
 }
